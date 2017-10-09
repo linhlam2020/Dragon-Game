af08a88b38f3f53a7eed149948d10ab8c094258e
linhlam2020 <32391015+linhlam2020@users.noreply.github.com>
2017-10-09 09:04:46 -0400

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -4,11 +4,13 @@
 *@author Team 4B : Linh Lam, So Negishi, Hoang Pham, Duc Nguyen
 *@version September 27, 2017
 */
+import java.util.ArrayList;
 
 
 public class Driver {
 
 	public static void main(String[] args) {
+		ArrayList<Item> list = new ArrayList<Item>();
 		Item i1 = new Item();
 		Item i2 = new Item("sandwich", "consumable", "a peanut butter and jelly sandwich");
 		Item i3 = new Item("croissant", "consumable", "a buttered flaky bread from France");
@@ -22,8 +24,20 @@ public class Driver {
 
 		i2.print();
 		i3.print();
-
-		System.out.println(i2.getName() + " " + i2.getType() + " " + i2.getDescription());
+		
+		Item i4 = new Item( "flashlight", "tool", "a small LED flashlight");
+		Item i5 = new Item( "gun", "weapon", "a combat shotgun" );
+		
+		
+		list.add( i1 );
+		list.add( i2 );
+		list.add( i3 );
+		list.add( i4 );
+		list.add( i5 );
+		
+		for ( Item i : list )
+			System.out.println( i );
 	}
 
 }
+
