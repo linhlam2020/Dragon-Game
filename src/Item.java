6542b7a0e7b1895fee4cfdb52065360b7c8f0c30
skyair55 <sonegishi_2020@depauw.edu>
2017-10-09 09:33:12 -0400

    Periodical Updates

--- src/Item.java
+++ src/Item.java
@@ -7,75 +7,70 @@
 
 public class Item 
 {
-  private String name; 
-		private String type;
-		private String description;
+	private String name; 
+	private String type;
+	private String description;
 		
-		// A constructor that takes three parameters and
-		// sets the values accordingly. Parameters must
-		// be in order: name, type, description.
-		
-		public Item( String iName, String iType, String iDescription)
-		{
-			name = iName;
-			type = iType;
-			description = iDescription;
-		}
-		
-		
-		// This constructor takes no parameters and
-		// sets variables to arbitrary values.
-		public Item( )
-		{
-			name = "stick";
-			type = "tool";
-			description = "a long bamboo stick";
-		}
-		
-		
-		
-		// Set methods for each variable.
-		
-		public void setName( String iName )
-		{
-			name = iName;
-		}
+	// A constructor that takes three parameters and
+	// sets the values accordingly. Parameters must
+	// be in order: name, type, description.
+	
+	public Item( String iName, String iType, String iDescription)
+	{
+		name = iName;
+		type = iType;
+		description = iDescription;
+	}
+	
+	
+	// This constructor takes no parameters and
+	// sets variables to arbitrary values.
+	public Item( )
+	{
+		name = "stick";
+		type = "tool";
+		description = "a long bamboo stick";
+	}
+	
+	
+	// Set methods for each variable.
+	public void setName( String iName )
+	{
+		name = iName;
+	}
 
-		public void setType( String iType )
-		{
-			type = iType;
-		}
-		
-		public void setDescription( String iDescription )
-		{
-			description = iDescription;
-		}
-		
-		
-		// Get methods for each variable.
-		
-		public String getName( )
-		{
-			return name;
-		}
+	public void setType( String iType )
+	{
+		type = iType;
+	}
+	
+	public void setDescription( String iDescription )
+	{
+		description = iDescription;
+	}
+	
+	
+	// Get methods for each variable.
+	public String getName( )
+	{
+		return name;
+	}
 
-		public String getType( )
-		{
-			return type;
-		}
-		
-		public String getDescription( )
-		{
-			return description;
-		}
-		
-		
-		// To String method: set the template to prints out the information about the item.
-		
-		public String toString( )
-		{
-			String s = "Item: " + "\n" + "      " + "shortname: " + name + "\n" + "      "+ "type: " + type + "\n" + "      " + "description: " + description + "\n";
-			return s;
-		}
+	public String getType( )
+	{
+		return type;
+	}
+	
+	public String getDescription( )
+	{
+		return description;
+	}
+	
+	
+	// To String method: set the template to prints out the information about the item.
+	public String toString( )
+	{
+		return "Item:\n" + "Shortname: " + name + "\n" + "      "+ "Type: " + type + "\n" + "Description: " + description + "\n";
+	}
 
 }
