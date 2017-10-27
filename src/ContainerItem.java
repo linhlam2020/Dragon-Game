eb54a1e8165e5c97626cbe92b414be73fc6ef214
skyair55 <sonegishi_2020@depauw.edu>
2017-10-27 08:28:18 -0400

    Updates

--- src/ContainerItem.java
+++ src/ContainerItem.java
@@ -1,2 +1,15 @@
+/*
+*@description Location class
+*
+*@author Team 4B : Linh Lam, So Negishi, Hoang Pham, Duc Nguyen
+*@version October 27, 2017
+*/
+
 public class ContainerItem extends Item {
+
+
+    public ContainerItem() {
+
+    }
+
 }
